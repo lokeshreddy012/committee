a0b5ef31810eacc8c329af76c1a80906c10bd02d
lokeshreddy012 <165644105+lokeshreddy012@users.noreply.github.com>
2024-09-18 16:15:46 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -2,13 +2,14 @@ const express = require('express');
 const { MongoClient } = require('mongodb');
 const usersRoutes = require('./routes/users');
 const committeesRoutes = require('./routes/committees');
+require('dotenv').config();
 
 const app = express();
 const PORT = process.env.PORT || 5000;
 
 app.use(express.json());
 
-const uri = 'mongodb+srv://lokesh:lokesh@cluster1.edory.mongodb.net/committeesDB';  // Your actual MongoDB URI
+const uri = process.env.URI;  // Your actual MongoDB URI
 let db, usersCollection, committeesCollection;
 
 // Connect to MongoDB
